b28222eced543c77901aa59487a06ddb3e702b15
caopeng <caopeng@youzan.com>
2021-11-02 12:49:57 +0800

    feat:接口补充

--- src/main/java/org/sot/project/controller/OperationController.java
+++ src/main/java/org/sot/project/controller/OperationController.java
@@ -39,6 +39,7 @@ public class OperationController {
 
     @Resource
     private InformationService informationService;
+
     //活动部分接口
     @PostMapping(value = "/activity")
     @ApiOperation(value = "创建活动")
@@ -48,12 +49,33 @@ public class OperationController {
     }
 
     @GetMapping("/activity/{userId}")
-    @ApiOperation(value = "活动列表查询", notes = "")
+    @ApiOperation(value = "活动列表查询 通过用户id", notes = "")
     @ApiImplicitParams({@ApiImplicitParam(name = "userId", value = "用户Id", dataType = DataType.STRING, paramType = ParamType.PATH)})
-    public ApiResponse<List<Activity>> getActivities(@PathVariable String userId) {
+    public ApiResponse<List<Activity>> getActivitiesByUserId(@PathVariable String userId) {
         log.info("单个参数用  @ApiImplicitParam");
         return ApiResponse.<List<Activity>>builder().code(200).message("操作成功").data(new ArrayList()).build();
     }
+
+    @GetMapping("/activity/all")
+    @ApiOperation(value = "查询所有活动列表", notes = "")
+    public ApiResponse<List<Activity>> getActivities() {
+        log.info("单个参数用  @ApiImplicitParam");
+        return ApiResponse.<List<Activity>>builder().code(200).message("操作成功").data(new ArrayList()).build();
+    }
+
+    @GetMapping("/activity/{activityId}")
+    @ApiOperation(value = "查询单个活动内容", notes = "")
+    @ApiImplicitParams({@ApiImplicitParam(name = "activityId", value = "活动id", dataType = DataType.STRING, paramType = ParamType.PATH)})
+    public ApiResponse<List<Activity>> getActivitiesByActivityId(@PathVariable String activityId) {
+        log.info("单个参数用  @ApiImplicitParam");
+        //查活动 获取活动信息
+        //查留言 获取留言信息
+        //查用户信息
+        return ApiResponse.<List<Activity>>builder().code(200).message("操作成功").data(new ArrayList()).build();
+    }
+
+
+
 
 
     // 留言部分接口
@@ -62,7 +84,7 @@ public class OperationController {
     public ApiResponse<Information> postComment(@RequestBody InformationDTO informationDTO) {
         return WebUtils.process(()->informationService.postComment(informationDTO));
     }
-
+    //做一个分页接口
     @GetMapping("/comments/{userId}")
     @ApiOperation(value = "留言列表查询", notes = "")
     @ApiImplicitParams({@ApiImplicitParam(name = "userId", value = "用户Id", dataType = DataType.STRING, paramType = ParamType.PATH)})
@@ -70,10 +92,6 @@ public class OperationController {
         log.info("单个参数用  @ApiImplicitParam");
         return ApiResponse.<List<Comment>>builder().code(200).message("操作成功").data(new ArrayList()).build();
     }
-
-
-
-
 
 
 
