58894297b52e03f7293a3e22382748d87eb707f1
gmw <gaominwen99@163.com>
2021-11-07 17:03:03 +0800

    名称修改

--- src/main/java/org/sot/project/dao/mapper/InformationMapper.java
+++ src/main/java/org/sot/project/dao/mapper/InformationMapper.java
@@ -12,7 +12,7 @@ import javax.sound.sampled.Line;
  * @date 2021/11/2 10:25 上午
  */
 @Mapper
-public interface InformationDAO {
+public interface InformationMapper {
 
 	int saveInformation(InformationDO informationDO);
 
--- src/main/java/org/sot/project/service/InformationService.java
+++ src/main/java/org/sot/project/service/InformationService.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.annotation.Resource;
 import org.sot.project.controller.dto.CommentDTO;
@@ -13,9 +12,9 @@ import org.sot.project.controller.dto.InformationDTO;
 import org.sot.project.dao.dataobject.CommentDO;
 import org.sot.project.dao.dataobject.InformationDO;
 import org.sot.project.dao.dataobject.UserLikeDO;
-import org.sot.project.dao.mapper.CommentDAO;
-import org.sot.project.dao.mapper.InformationDAO;
-import org.sot.project.dao.mapper.UserLikeDAO;
+import org.sot.project.dao.mapper.CommentMapper;
+import org.sot.project.dao.mapper.InformationMapper;
+import org.sot.project.dao.mapper.UserLikeMapper;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,13 +26,13 @@ import org.springframework.stereotype.Service;
 public class InformationService {
 
 	@Resource
-	private InformationDAO informationDAO;
+	private InformationMapper informationDAO;
 
 	@Resource
-	private UserLikeDAO userLikeDAO;
+	private UserLikeMapper userLikeDAO;
 
 	@Resource
-	private CommentDAO commentDAO;
+	private CommentMapper commentDAO;
 
 	public InformationDTO createInformation(InformationDTO informationDTO){
 		//TODO:参数校验 增加根据类型的校验
